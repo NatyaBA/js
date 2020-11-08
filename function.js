4fe8bd2f96165615a4af3be7ea5f4198841429fe
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2020-11-08 13:36:19 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,61 @@
+function sum(a, b) {
+    var f = a + b;
+    return f;
+}
+
+function sub(a, b) {
+    var f = a - b;
+    return f;
+}
+
+function mul(a, b) {
+    var f = a * b;
+    return f;
+}
+
+function div(a, b) {
+    var f = a / b;
+    return f;
+}
+
+function persent(c,d) {
+    var f = (c*d)/100;
+    return f;
+}
+
+function integerDiv(c, d) {
+    return Math.floor (c / d);
+}
+
+function abs(c) {
+    return Math.abs (c);
+}
+
+function sqrt(c) {
+    return Math.sqrt (c);
+}
+
+function factorial (c) {
+    return Math.factorial (c);
+}
+
+function round(c) {
+    return Math.round(c);
+  }
+
+function sin(c) {
+    return Math.sin(c);
+}
+
+function cos(c) {
+    return Math.cos(c);
+}
+
+function tg(c) {
+    return Math.tg(c);
+}
+
+function ctg(c) {
+    return Math.ctg(c);
+}
+
