9069e73ad780da191e333ae1fa9f9bb7ddb6e69c
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2020-11-13 09:39:14 +0300

    Add files via upload

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 function sum(a, b) {
-    return(a + b);
+    return a + b;
 }
 
 function sub(a, b) {
@@ -8,38 +8,37 @@ function sub(a, b) {
 }
 
 function mul(a, b) {
-    return(a * b);
+    return a * b;
 }
 
 function div(a, b) {
-    return (a / b);
+    return  a / b;
 }
 
 function persent(c,d) {
-    return ((c*d)/100);
+    return (c*d)/100;
 }
 
 function integerDiv(c, d) {
-    return ((c - c % d) / d);
+    return (c - c % d) / d;
 }
 
 function abs(c) {
     if (c < 0) {
         c *= -1;
     }
-    return (c);
+    return c;
 }
 
 function sqrt(c) {
-    return (c ** (1/2));
+    return c ** (1/2);
 }
 
 function factorial(z) {
-    result = 1;
     for (i = 1; i <= z; i++) {
         result = result * i;
     }
-return (result);
+return result;
 }
     
 
@@ -50,76 +49,42 @@ function round(c) {
           c-=f+1;
         }
         if ((c%1) < 0.5){
-        f=((c - c % 1) / 1);  
+        f=(c - c % 1);  
         c-=f;
       }}
-return(c);
+return c;
 }
 
 function sin(x, z) {
-    Sin = 1;
-    n = 0;
-    term = 1;
     for (i = 1; i <= 2*z; i++) {
         n += 2;
         term = term *(-x*x)/ ( n*(n+1) );
         Sin += term;
     }
     Sin *= x;
-    return(Sin);
+    return Sin;
 }
 
 function cos(x, z) {
-    s = 0;
-    n = 0;
-    term = 1
     while(term > z) {
       s += term;
       n++;
       term *= -x * x / (2 * n - 1) / (2 * n)
     }
-    return (s);
+    return s;
   }
    
 /*    TG         */
 
-function tg(x, z) {
-    Sin = 1;
-    n = 0;
-    term = 1;
-    for (i = 1; i <= 2*z; i++) {
-        n += 2;
-        term = term *(-x*x)/ ( n*(n+1) );
-        Sin += term;
-    }
-    Sin *= x;
-s = 0;
-while(term > z) {
-  s += term;
-  n++;
-  term *= -x * x / (2 * n - 1) / (2 * n)
-}
-return (Sin/s)
+function tg(z) {
+    z=sin.Sin/ cos.s;
+return z;
 }
 
 /*   CTG         */
 
-function ctg(x, z) {
-    Sin = 1;
-    n = 0;
-    term = 1;
-    for (i = 1; i <= 2*z; i++) {
-        n += 2;
-        term = term *(-x*x)/ ( n*(n+1) );
-        Sin += term;
-    }
-    Sin *= x;
-s = 0;
-while(term > z) {
-  s += term;
-  n++;
-  term *= -x * x / (2 * n - 1) / (2 * n)
-}
-return (s/Sin)
+function ctg(z) {
+    z=cos.s/sin.Sin;
+return z;
 }
 
